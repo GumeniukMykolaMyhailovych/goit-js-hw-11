a553fd8f059ab6395a8ce31efd166e56bdf88fac
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-01-16 15:40:29 +0200

    Correct Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -12,43 +12,32 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    
+
     root: 'src',
-    
+
     build: {
       sourcemap: true,
+      outDir: '../dist',
+      emptyOutDir: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
+        
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: chunkInfo => chunkInfo.name === 'commonHelpers' ? 'commonHelpers.js' : '[name].js',
+          assetFileNames: assetInfo => assetInfo.name && assetInfo.name.endsWith('.html') ? '[name].[ext]' : 'assets/[name]-[hash][extname]',
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    
+
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      SortCss({ sort: 'mobile-first' }),
     ],
   };
 });
