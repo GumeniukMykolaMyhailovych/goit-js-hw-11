103721489db31ed7a8f76cca6d89e71e41da516a
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-01-16 16:21:11 +0200

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,20 @@ import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => ({
-  // base для GitHub Pages
+
   base: command === 'serve' ? '/' : '/goit-js-hw-11/',
 
-  // папка з твоїм кодом
   root: 'src',
 
   build: {
-    // результат збірки у корень проекту dist
     outDir: '../dist',
     emptyOutDir: true,
 
     rollupOptions: {
-      // всі html файли у src автоматично збираються
       input: glob.sync('./src/*.html'),
 
       output: {
-        // всі assets (css, зображення, шрифти) у папку assets з хешем
         assetFileNames: 'assets/[name]-[hash][extname]',
-        // JS файли з хешем
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
       },
@@ -31,11 +26,8 @@ export default defineConfig(({ command }) => ({
   },
 
   plugins: [
-    // інжекція змінних у html
     injectHTML(),
-    // live reload при зміні html
     FullReload(['./src/**/*.html']),
-    // сортування медіа-запитів у css
     SortCss({ sort: 'mobile-first' }),
   ],
 }));
